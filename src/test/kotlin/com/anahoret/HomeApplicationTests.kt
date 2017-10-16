3fab71fde45f7d09bfa22e9d879999fa12fe130d
Alexander Mikhalchenko <mikhalchenko.alexander@gmail.com>
2017-10-16 21:36:01 +0300

    Fix test.

--- src/test/kotlin/com/anahoret/HomeApplicationTests.kt
+++ src/test/kotlin/com/anahoret/HomeApplicationTests.kt
@@ -1,12 +1,13 @@
 package com.anahoret
 
+import com.anahoret.home_server.HomeApplication
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(classes = arrayOf(HomeApplication::class))
 class HomeApplicationTests {
 
 	@Test
